f4394844603c0badd10d8523717427306d30708c
Pranay Kumar <pranaykumar074@gmail.com>
2023-08-16 15:09:16 +0530

    Python function and main file updated

--- CAPM_Return.py
+++ CAPM_Return.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import yfinance as yf
 import pandas_datareader.data as web
-import capm_function 
+import capm_function
 
 st.set_page_config(page_title = "CAPM",
     page_icon = "chart_with_upwards_trend",
@@ -22,50 +22,98 @@ with col2:
 
 
 ## Downloading data for SP500
+try:
+    end = datetime.date.today()
+    start = datetime.date(datetime.date.today().year-year, datetime.date.today().month, datetime.date.today().day)
 
-end = datetime.date.today()
-start = datetime.date(datetime.date.today().year-year, datetime.date.today().month, datetime.date.today().day)
+    SP500 = web.DataReader(['sp500'],'fred',start,end)
 
-SP500 = web.DataReader(['sp500'],'fred',start,end)
+    # print(SP500.tail())
 
-# print(SP500.tail())
+    ##for storing the Close value of all the stock
+    close=[]
+    stocks_df = pd.DataFrame()
+    for stock in stocks_list:
+        data = yf.download(stock,period = f'{year}y')
+        # print(data.tail())
+        stocks_df[f'{stock}'] = data['Close']
+        
+    # print(stocks_df.head())
+    stocks_df.reset_index(inplace = True)
+    SP500.reset_index(inplace = True)
+    # print(stocks_df.dtypes)
+    # print(SP500.dtypes)
 
-##for storing the Close value of all the stock
-close=[]
-stocks_df = pd.DataFrame()
-for stock in stocks_list:
-    data = yf.download(stock,period = f'{year}y')
-    # print(data.tail())
-    stocks_df[f'{stock}'] = data['Close']
+    SP500.columns =['Date','sp500']
+    ## Making the datetype of all the columns same
+    stocks_df['Date'] = stocks_df['Date'].apply(lambda x: str(x)[:10])
+    stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
+    ## Merging the dataframes
+
+    stocks_df = pd.merge(stocks_df, SP500, on='Date', how = 'inner')
+
+    # print(stocks_df)
+
+    col1, col2 = st.columns([1,1])
+    with col1:
+        st.markdown('### Dataframe head')
+        st.dataframe(stocks_df.head(), use_container_width=True)
+        
+    with col2:
+        st.markdown('### Dataframe tail')
+        st.dataframe(stocks_df.tail(), use_container_width=True)
+        
+    col1, col2 = st.columns([1,1])
+
+    with col1:
+        st.markdown('### Price of all the Stocks')
+        st.plotly_chart(capm_function.interactive_plot(stocks_df))
+
+    with col2:
+        new_df = capm_function.normalize_prices(stocks_df)
+        st.markdown('### Price of all the Stocks (After Normalizing)')
+        st.plotly_chart(capm_function.interactive_plot(new_df))
+        
+    stocks_daily_return = capm_function.daily_return(stocks_df)
+    # print(stocks_daily_return)
+
+    beta = {}
+    alpha = {}
+
+    for i in stocks_daily_return.columns:
+        if i != 'Date' and i != 'sp500':
+            b, a = capm_function.calculate_beta(stocks_daily_return, i)
+            
+            beta[i] =b
+            alpha[i] =a
+    # print(beta, alpha)
+
+    beta_df = pd.DataFrame(columns=['Stock','Beta Value'])
+    beta_df['Stock'] = beta.keys()
+    beta_df['Beta Value'] = [str(round(i, 2)) for i in beta.values()]
+
+    # col1, col2 = st.columns([1,1])
+
+    with col1:
+        st.markdown('### Calculated Beta Value')
+        st.dataframe(beta_df,use_container_width=True)
     
-# print(stocks_df.head())
-stocks_df.reset_index(inplace = True)
-SP500.reset_index(inplace = True)
-# print(stocks_df.dtypes)
-# print(SP500.dtypes)
+    rf = 0
+    rm = stocks_daily_return['sp500'].mean()*252 
 
-SP500.columns =['Date','sp500']
-## Making the datetype of all the columns same
-stocks_df['Date'] = stocks_df['Date'].apply(lambda x: str(x)[:10])
-stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
-## Merging the dataframes
+    return_df = pd.DataFrame()
+    return_value = []
+    for stock, value in beta.items():
+        return_value.append(str(round(rf+(value*(rm-rf)),2))) 
+    return_df['Stock'] = stocks_list
 
-stocks_df = pd.merge(stocks_df, SP500, on='Date', how = 'inner')
+    return_df['Return Value'] = return_value
 
-# print(stocks_df)
+    with col2:
+        st.markdown('### Calculated Return using CAPM')
+        
+        st.dataframe(return_df,use_container_width=True)
 
-col1, col2 = st.columns([1,1])
-with col1:
-    st.markdown('### Dataframe head')
-    st.dataframe(stocks_df.head(), use_container_width=True)
-    
-with col2:
-    st.markdown('### Dataframe tail')
-    st.dataframe(stocks_df.tail(), use_container_width=True)
-    
-col1, col2 = st.columns([1,1])
-
-with col1:
-    st.markdown('### Price of all the Stocks')
-    st.plotly_chart(capm_function.interactive_plot(stocks_df))
+except:
+    st.write('Please Select the valid input')
     
